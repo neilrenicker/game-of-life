bd81a94c244adcb220f02f62f1f3427d8a13edf2
Neil Renicker <neilrenicker@gmail.com>
2013-02-19 09:14:54 -0500

    Separate spec from game, rename spec file

---
+++ spec/spec.rb
@@ -0,0 +1,32 @@
+# Run rspec tests
+
+describe 'Cell' do
+  it "should return not be alive if neighbors < 2" do
+    cell = Cell.new(4,5,1)
+    cell.alive.should == false
+  end
+
+  it "should return not be alive if neighbors > 3" do
+    cell = Cell.new(4,5,5)
+    cell.alive.should == false
+  end
+
+  it "should return true if neighbors = 2 or 3" do
+    cell = Cell.new(4,5,2)
+    cell.alive.should == true
+  end
+
+  it "should be born if neighbors = 3" do
+    cell = Cell.new(4,5,3)
+    cell.born.should == true
+  end
+
+  it "should have a neighbors with x and y values based on their relationship to it" do
+    cell = Cell.new(0,0,2)
+    cell.north.should == [0,1]
+    cell.south.should == [0,-1]
+    cell.east.should == [1,0]
+    cell.west.should == [-1,0]
+  end
+
+end
